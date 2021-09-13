40ef5a5aa4ba964fd236db82b92b0d66ce7cdb18
BuildTools <unconfigured@null.spigotmc.org>
2021-09-13 16:00:42 +0200

    get products

--- auction/routes/products.js
+++ auction/routes/products.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const fs = require("fs");
+const util = require("util");
 const router = express.Router();
+const StatusCodes = require("http-status-codes");
 
-/*
-code
- */
-router.get('/', async(res, req, next) => {
+const readFilePromise = util.promisify(fs.readFile);
 
+
+router.get('/', async (req, res) => {
+    let products;
+    try {
+        products = await readFilePromise('./storage/products.json');
+        products = JSON.parse(products);
+        console.log("read products.json")
+        await res.status(StatusCodes.OK)
+    } catch (e) {
+        console.error(e);
+        await res.status(StatusCodes.BAD_REQUEST)
+    }
+    res.send(products);
 })
 
 module.exports = router;
