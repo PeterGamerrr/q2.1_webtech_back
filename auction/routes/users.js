cadefcc81df1ed579c49e0cd1a18d5c53b2c23cb
BuildTools <unconfigured@null.spigotmc.org>
2021-09-13 16:12:33 +0200

    reset users

--- auction/routes/users.js
+++ auction/routes/users.js
@@ -3,19 +3,9 @@ const router = express.Router();
 const debug = require('debug')('auction:server');
 const fs = require('fs');
 
-//example (temp)
-fs.readFile('./storage/users.json', (err, data) => {
-  if (err) throw err;
-  let users = JSON.parse(data);
-})
 
+router.get('/', function(req, res) {
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200)
-  res.json(users);
 });
-
-//todo: write a saving script on shutdown
 
 module.exports = router;
