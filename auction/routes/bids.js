e3aa36318a49da0e146778880aa6a7f79dc149a3
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-09-13 17:02:12 +0200

    Bids get route

--- auction/routes/bids.js
+++ auction/routes/bids.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const StatusCodes = require("http-status-codes");
+let bids = require("../storage/bids")
 
-/*
-code
- */
+router.get('/', (req, res) => {
+    res
+        .status(StatusCodes.OK)
+        .send(bids);
+})
 
 module.exports = router;
