e6c210f4a5dfdc94984045f7ae5e2cebb4eb6946
Emanuel-de-Jong <emanueldejong@hotmail.com>
2021-09-13 16:51:01 +0200

    Storage products and bids example data

--- auction/storage/bids.js
+++ auction/storage/bids.js
@@ -1,5 +1,18 @@
-export let bids = [
-
+let bids = [
+    {
+        id: 0,
+        price: 30.2,
+        hasWon: false,
+        userId: 0,
+        productId: 0,
+    },
+    {
+        id: 1,
+        price: 25.6,
+        hasWon: true,
+        userId: 1,
+        productId: 1,
+    },
 ]
 
 module.exports = bids;
--- auction/storage/products.js
+++ auction/storage/products.js
@@ -1,8 +1,22 @@
 let products = [
   {
-    productId:0,
-    productName:"bier"
-  }
+    id: 0,
+    name: "Heineken Oud Bruin",
+    startPrice: 28,
+    endDate: new Date(1977, 0).getTime(),
+    region: "Amsterdam",
+    brand: "Heineken",
+    capacity: 33,
+  },
+  {
+    id: 1,
+    name: "Grolsch Stender Alcoholarm",
+    startPrice: 42,
+    endDate: new Date(1990, 0).getTime(),
+    region: "Enschede",
+    brand: "Grolsch",
+    capacity: 33,
+  },
 ]
 
 module.exports = products;
