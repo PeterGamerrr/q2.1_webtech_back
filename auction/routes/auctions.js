b07045c3dd37a4f61ea1dbb51309b00c63f02cc7
Peter_Gamerrr <497155@student.saxion.nl>
2021-09-28 10:46:27 +0200

    made auction router (untested)

--- auction/routes/auctions.js
+++ auction/routes/auctions.js
@@ -1,12 +1,188 @@
 const express = require("express");
 const router = express.Router();
 const { StatusCodes } = require("http-status-codes");
-let { auctions, counter } = require("../storage/auctions");
+const isLoggedIn = require("../middleware/is-logged-in");
+const { hasAdmin } = require("../middleware/has-role");
+let { auctions, counter,fields ,fieldsToValidate } = require("../storage/auctions");
+const {products} = require("../storage/products");
+
 
 router.get("/", (req, res) => {
+    let auctionsToSend = null;
+
+    if (Object.keys(req.query).length === 0) {
+        auctionsToSend = auctions;
+    } else {
+        auctionsToSend = auctions.filter(auction => {
+            for (const [key, val] of Object.entries(req.query)) {
+                if (auction[key] !== undefined && auction[key].toLowerCase() !== val.toLowerCase()) {
+                    return false;
+                }
+            }
+
+            return true;
+        })
+    }
+
     res
         .status(StatusCodes.OK)
-        .send(auctions);
+        .send(auctionsToSend);
 });
+
+
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    let auction = auctions.find(auction => auction.id == id);
+    if (auction === undefined) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not found");
+    }
+
+    res
+        .status(StatusCodes.OK)
+        .send(auction);
+});
+
+
+router.post("/", (req, res) => {
+    let newAuction = req.body;
+
+
+    if (!checkAuctionValidity(newAuction, true)) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not valid");
+    }
+
+    counter++;
+    newAuction.id = counter;
+
+    auctions.push(newAuction);
+    res
+        .status(StatusCodes.CREATED)
+        .send(newAuction);
+});
+
+
+router.put("/:id", isLoggedIn, hasAdmin, (req, res) => {
+    let id = req.params.id;
+
+    let newAuction = req.body;
+    if (!checkAuctionValidity(newAuction, true)) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not valid");
+    }
+
+    let auction = auctions.find(auction => auction.id == id);
+    if (auction === undefined) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not found");
+    }
+
+    fieldsToValidate.forEach(field => {
+        if (newAuction[field] !== undefined) {
+            auction[field] = newAuction[field];
+        }
+    });
+
+    res
+        .status(StatusCodes.OK)
+        .send(auction);
+});
+
+
+router.patch("/:id", isLoggedIn, hasAdmin, (req, res) => {
+    let id = req.params.id;
+
+    let newAuction = req.body;
+    if (!checkAuctionValidity(auction)) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not valid");
+    }
+
+    let auction = auctions.find(auction => auction.id == id);
+    if (auction === undefined) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not found");
+    }
+
+    fieldsToValidate.forEach(field => {
+        if (newAuction[field] !== undefined) {
+            auction[field] = newAuction[field];
+        }
+    });
+
+    res
+        .status(StatusCodes.OK)
+        .send(auction);
+});
+
+
+router.delete("/:id", isLoggedIn, hasAdmin, (req, res) => {
+    let id = req.params.id;
+
+    let auctionIndex = auctions.findIndex(auction => auction.id == id);
+    if (auctionIndex == -1) {
+        return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send("Auction not found");
+    }
+    let auction = auctions[auctionIndex]
+
+
+    auctions.splice(auctionIndex, 1);
+
+    res
+        .status(StatusCodes.OK)
+        .send(auction);
+});
+
+
+function checkAuctionValidity(auction, allFields = false) {
+    let checkFields = {};
+    fieldsToValidate.forEach(field => {
+        checkFields[field] = false
+    });
+
+    for (const [key, val] of Object.entries(auction)) {
+        checkFields[key] = true;
+
+        if (key == "title" && (
+            typeof val !== "string" ||
+            val.length < 3 ||
+            val.length > 512)) {
+            return false;
+        }
+        else if (key == "productId" && (
+            typeof val !== "number" ||
+            val < 0 ||
+            products.findIndex(product => product.id == val) === -1)) {
+            return false;
+        }
+        else if (key == "endDate" && (
+            typeof val !== "number" ||
+            val < Date.now() - 30000)) {
+            return false;
+        }
+        else if (!fields.includes(key)) {
+            return false;
+        }
+    }
+
+    if (allFields) {
+        for (const val of Object.values(checkFields)) {
+            if (!val) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 
 module.exports = router;
--- auction/storage/auctions.js
+++ auction/storage/auctions.js
@@ -2,15 +2,22 @@ let auctions = [
     {
         id: 0,
         title: "Heineken Oud Bruin",
+        endDate: Date.now() + 600000,
         productId: 0
     },
     {
         id: 1,
         title: "Grolsch Stender",
+        endDate: Date.now() + 300000,
         productId: 1
     }
 ];
 
+const fieldsToValidate = ["title", "productId", "endDate"];
+
+const fields = ["id", "title","productId", "endDate"];
+
+
 let counter = auctions.length - 1;
 
-module.exports = { auctions, counter};
+module.exports = { auctions, counter,fields ,fieldsToValidate};
