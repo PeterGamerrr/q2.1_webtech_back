867c0569ef98475b6a95b3f5b2aafc0c3ccf4acb
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2021-10-13 10:46:49 +0200

    Add cors to package.json

--- auction/app.js
+++ auction/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
